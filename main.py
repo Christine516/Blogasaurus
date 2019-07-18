c5b48219be0e035a0021c009381f852fb21f3f4d
cssi <cssi@cssis-MacBook-Pro-4.local>
2019-07-18 10:38:15 -0400

    title, content, author

--- main.py
+++ main.py
@@ -12,7 +12,26 @@ class MainPage(webapp2.RequestHandler):
         template = jinja_env.get_template('static/templates/index.html')
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(template.render())
-
+class NewPost(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('static/templates/new_post.html')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render())
+class ViewPost(webapp2.RequestHandler):
+    def post(self):
+        title_variable = self.request.get('title')
+        content_variable = self.request.get('content')
+        author_variable = self.request.get('author')
+        view_dict = {
+        'title_var': title_variable,
+        'content_var': content_variable,
+        'author_var': author_variable,
+        }
+        template = jinja_env.get_template('static/templates/view_post.html')
+        self.response.write(template.render(view_dict))
+        print("You've submitted a new post")
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/post', NewPost),
+    ('/view', ViewPost),
 ], debug=True)
